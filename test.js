9c67dfe5f9b41c75d3dfc576f6c36af7aecfe563
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-03-17 12:05:50 -0400

    fdsgds

--- test.js
+++ test.js
@@ -18,7 +18,7 @@ const guestQueue = [
     { name: "Ryan", partySize: 3, rideChoices: ["Peter Pan's Flight", "Splash Mountain", "Guardians of the Galaxy: Cosmic Rewind"] }
 ];
 
-function getTime(timeFrame1) {
+function getTime(timeFrame1, timeFrame2) {
     let [start, finish] = timeFrame1.split(" - ", 2);
     console.log(start, finish);
     let [hours, minutes] = start.split(":", 2);
@@ -26,10 +26,28 @@ function getTime(timeFrame1) {
     hours = parseInt(hours);
     minutes = parseInt(minutesFinal);
     console.log(hours, minutes);
-    start = [hours, minutes];
+    start = hours + (minutes / 60);
+    let [hours1, minutes1] = finish.split(":", 2);
+    let minutesFinal1 = minutes1.split(" ", 1);
+    hours1 = parseInt(hours1);
+    minutes1 = parseInt(minutesFinal1);
+    console.log(hours1, minutes1);
+    finish = hours1 + (minutes1 / 60);
+    let [start2, finish2] = timeFrame2.split(" - ", 2);
+    console.log(start2);
+    let [hours3, minutes3] = start2.split(":", 2);
+    let minutesFinal3 = minutes3.split(" ", 1);
+    hours3 = parseInt(hours3);
+    minutes3 = parseInt(minutesFinal3);
+    console.log(hours3, minutes3);
+    start2 = hours3 + (minutes3 / 60);
+    console.log("hi", start, start2, finish);
+    if (start2 >= start && start2 <= finish) {
+        return false;
+    } else {
+        return true;
+    }
 }
-
-getTime("10:00 PM - 11:00 PM")
 
 function booking(guestQueue) {
     let bookings = [];
@@ -46,11 +64,27 @@ function booking(guestQueue) {
                 if (ride.name === guestRide) {
                     if (ride.availableSpots >= guest.partySize) {
                         if (rides < max) {
-                            bookedRides.push({ name: ride.name, time: ride.time });
-                            rides++;
-                            console.log(rides);
-                            console.log(bookedRides);
-                            ride.availableSpots = ride.availableSpots - guest.partySize;
+                            const time = ride.time;
+                            let a = false;
+                            console.log(1, time, bookedRides);
+                            bookedRides.forEach((ride1) => {
+                                console.log(ride1.time);
+                                let b = getTime(ride1.time, time);
+                                console.log(b);
+                                if (b === true) {
+                                    a = true;
+                                }
+                            })
+                            if (bookedRides.length === 0) {
+                                a = true;
+                            }
+                            if (a === true) {
+                                bookedRides.push({ name: ride.name, time: ride.time });
+                                rides++;
+                                console.log(rides);
+                                console.log(bookedRides);
+                                ride.availableSpots = ride.availableSpots - guest.partySize;
+                            }
                         }
                     } else { };
                 } else { };
@@ -62,3 +96,5 @@ function booking(guestQueue) {
     });
     console.log(bookings);
 }
+
+booking(guestQueue);
